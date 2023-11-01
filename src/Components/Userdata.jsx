acb4b82d8375999cde9f1ada26a1ee132c1da5ba
karanmalhii <karanmalhii1313@gmail.com>
2023-11-01 07:02:32 +0530

    useEffect hook with api calling

--- src/Components/Userdata.jsx
+++ src/Components/Userdata.jsx
@@ -1,33 +1,63 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 // import data from "../../data/users.json";
 import Data from "../Components/Data";
 
 export default function Userdata() {
-  
   // console.log(data.users)
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(function(){
-    console.log("only called once when componenet is loaded! : array hai pr bilkul khali hai")
+  async function getUsers() {
+    setLoading(true);
+
+    let response = await fetch("https://fakestoreapi.com/users");
+    // console.log(response);
+    let responseData = await response.json();
+    // console.log(responseData);
+    
+    setData(responseData);
+    setLoading(false);
+  }
+
+  useEffect(function () {
+    getUsers();
+    // console.log("only called once when componenet is loaded! : array hai pr bilkul khali hai")
   }, []);
 
-
-  useEffect(function(){
-    console.log("Called everytime when component is re-render : array hai hi nhi")
+  useEffect(function () {
+    // console.log("Called everytime when component is re-render : array hai hi nhi")
   });
 
+  useEffect(
+    function () {
+      // console.log("only called when value of count is changed! : Array vich value hai");
+      getUsers();
+    },
+    [count]
+  );
 
-  useEffect(function(){
-    console.log("only called when value of count is changed! : Array vich value hai")
-  }, [count]);
-
-  console.log(count)
+  console.log(data);
   return (
     <div>
-      <input onChange={(event) => setInput(event.target.value)} value={input}/>
+      <input onChange={(event) => setInput(event.target.value)} value={input} />
       <button onClick={() => setCount(count + 1)}>Click : {count}</button>
       {/* <Data mydata={data.users} /> */}
+
+      {/* <hr />
+      {loading && (<h2>Loading....</h2>) }
+      <hr /> */}
+
+      {loading == true ? (
+        <h2>Loading....</h2>
+      ) : (
+        <ul>
+          {data?.map(function (value, index) {
+            return <li>{value.username}</li>;
+          })}
+        </ul>
+      )}
     </div>
   );
 }
