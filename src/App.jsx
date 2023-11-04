a4dc87c75dd36907faca9e7305c087ebcc332430
Bikramjeet singh <bs863313@gmail.com>
2023-11-04 11:11:08 +0530

    working correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 // import Userdata from './Components/Userdata'
-import TestComponenet from "./Components/TestComponenet";
+import TestComponenet from "./Components/testComponenet"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 
@@ -15,6 +15,7 @@ function App() {
     {/* <h2>This is app.jsx</h2> */}
     {/* <Button variant="primary">Primary</Button>{' '} */}
       {/* <Userdata/> */}
+      <p>Bikram work</p>
       <TestComponenet/>
     </>
   )
