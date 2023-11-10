565967dc865f3cf6fa0b0acd5b84cc8ea4d3c719
Bikramjeet singh <bs863313@gmail.com>
2023-11-10 06:18:58 +0530

    work on task

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,36 +1,38 @@
 import React, { useState } from "react";
 
 export default function Form() {
-    const [count,setCount] = useState("");
+  const [data, setData] = useState("");
+
+
   return (
     <div>
-      <form>
+      <form onSubmit={(e) => setData(e.target.value)}>
         <div className="row">
-          <label htmlFor="">First name:</label>
-          <input type="text" name="" id="fname" />
+          <label htmlFor="fname">First name:</label>
+          <input type="text" name="fname" id="fname" />
         </div>
 
         <div className="row">
-          <label htmlFor="">Last name:</label>
-          <input type="text" name="" id="lname" />
+          <label htmlFor="lname">Last name:</label>
+          <input type="text" name="lname" id="lname" />
         </div>
 
         <div className="row">
-          <label htmlFor="">Age:</label>
-          <input type="number" name="" id="age" />
+          <label htmlFor="age">Age:</label>
+          <input type="number" name="age" id="age" />
         </div>
 
         <div className="row">
           <label htmlFor="">Gender:</label>
-          <label htmlFor="">male</label>
-          <input type="radio" name="" id="male" />
-          <label htmlFor="">female</label>
-          <input type="radio" name="" id="female" />
+          <label htmlFor="male">male</label>
+          <input type="radio" name="male" id="male" />
+          <label htmlFor="female">female</label>
+          <input type="radio" name="female" id="female" />
         </div>
 
         <div className="row">
-            <label htmlFor="">City:</label>
-          <select name="" id="city">
+          <label htmlFor="city">City:</label>
+          <select name="city" id="city">
             <option value="">Asr</option>
             <option value="">Jal</option>
             <option value="">Ludh</option>
@@ -39,7 +41,12 @@ export default function Form() {
         </div>
 
         <div className="btn">
-          <input type="submit" value="Submit" id="btn"/>
+          <input
+            type="submit"
+            value="Submit"
+            id="btn"
+            onSubmit={(e) => setData(e.target.value)}
+          />
         </div>
       </form>
     </div>
